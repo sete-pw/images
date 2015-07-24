430cb6c373e819f378665e1b8956215dfa5e3c2d
smile <smile.nikitoz@gmail.com>
2015-07-24 21:13:24 +0400

    [API] json-image

--- private/json/get-list.php
+++ private/json/get-list.php
@@ -1,2 +1,21 @@
 <?
-	
+$data = CO::RE()->get;
+$dataReturn = json_decode('{}');
+
+$dataReturn->status = json_decode('{}');
+
+if (isset($data['category'])){
+    $query = CO::SQL()->query("
+SELECT id_image, CONCAT('/image/',url,'_small') as url, category, transition, user_id
+FROM images
+WHERE category = ?
+    ",[
+        ['s',strip_tags(trim($data['category']))]
+    ]);
+    $dataReturn->status = 'success';
+    $dataReturn->responce = $query;
+    echo json_encode($dataReturn);
+}else{
+    $dataReturn->status = 'error';
+    echo json_encode($dataReturn);
+}
