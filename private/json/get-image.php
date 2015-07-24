8baa51481871221a9f4451a67b34607ed67652b0
smile <smile.nikitoz@gmail.com>
2015-07-24 22:12:26 +0400

    [API] transition-image

--- private/json/get-image.php
+++ private/json/get-image.php
@@ -1,2 +1,35 @@
 <?
+$data = CO::RE()->get;
+$dataReturn = json_decode('{}');
+$dataReturn->status = json_decode('{}');
+
+if (isset($data['id_image'])){
+    $query = CO::SQL()->query("
+SELECT id_image, CONCAT('/image/',url) as url, transition, user_id
+FROM images
+WHERE id_image = ?
+    ",[
+        ['i',(int)strip_tags(trim($data['id_image']))]
+    ]);
+
+    if (count($query)==0){
+        $dataReturn->status = 'error';
+        echo json_encode($dataReturn);
+    }else{
+        CO::SQL()->query("
+UPDATE images
+SET transition = transition + 1
+WHERE id_image = ?
+    ",[
+            ['i',(int)strip_tags(trim($data['id_image']))]
+        ]);
+        $dataReturn->status = 'success';
+        $dataReturn->responce = $query;
+        echo json_encode($dataReturn);
+    }
+}else{
+    $dataReturn->status = 'error';
+    echo json_encode($dataReturn);
+}
+
 	
--- private/json/get-list.php
+++ private/json/get-list.php
@@ -1,16 +1,15 @@
 <?
 $data = CO::RE()->get;
 $dataReturn = json_decode('{}');
-
 $dataReturn->status = json_decode('{}');
 
 if (isset($data['category'])){
     $query = CO::SQL()->query("
-SELECT id_image, CONCAT('/image/',url,'_small') as url, category, transition, user_id
+SELECT id_image, CONCAT('/preview/',url) as url, transition
 FROM images
 WHERE category = ?
     ",[
-        ['s',strip_tags(trim($data['category']))]
+        ['s',mb_strtolower(strip_tags(trim($data['category'])))]
     ]);
     $dataReturn->status = 'success';
     $dataReturn->responce = $query;
