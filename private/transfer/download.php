1638ecc5d5609222f6654fdc2d229e2bb774fb2c
smile <smile.nikitoz@gmail.com>
2015-07-24 23:55:06 +0400

    download

--- private/transfer/download.php
+++ private/transfer/download.php
@@ -1,10 +1,17 @@
 <?
 	$url = $args['url']; // url картинки из базы
 	$format = $args['format']; // preview или origin.
+	if ($format == 'preview'){
+
+		header('Content-Type: image/jpeg');
+		readfile(DIR_PRIVATE.'image-preview/'.$url.'.jpeg');
+		//echo file_get_contents(DIR_PRIVATE.'image-preview/'.$url.'.gif');
+	}
+
 
 	/**
 	 *
-	 * 
+	 *
 	 * Если preview, то отдаешь файл с именем $url из private/image
 	 * Иначе, из private/image-preview
 	 *
